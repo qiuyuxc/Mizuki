9712e25caa46faf8abaacfd8903176ae7f20d515
qiuyuxc <3788020380@qq.com>
2025-12-11 10:53:30 +0800

    Update 1 file: config.ts

--- src/config.ts
+++ src/config.ts
@@ -335,7 +335,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
-	name: "Qiuyu xc",
+	name: "Qiuyu",
 	bio: "The world is big, you have to go and see",
 	typewriter: {
 		enable: true, // 启用个人简介打字机效果
