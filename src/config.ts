23c5b6621190cee388cfe26224fa964c38373f4c
qiuyuxc <3788020380@qq.com>
2025-12-19 23:14:02 +0800

    Update 2 files: config.ts, 777.md

--- src/config.ts
+++ src/config.ts
@@ -23,7 +23,7 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: "Qiuyu Blog",
 	subtitle: "日记",
-	siteURL: "https://quiyu.cn/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://blog.quiyu.cn/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-01-01", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
