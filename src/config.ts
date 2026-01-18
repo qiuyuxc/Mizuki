8c1e765a9f5ae7734305a755ee3ae2f700fc84da
qiuyuxc <3788020380@qq.com>
2026-01-18 09:42:49 +0800

    Update 3 files: MiSans-Bold.ttf, config.ts, main.css

--- src/config.ts
+++ src/config.ts
@@ -196,9 +196,9 @@ export const siteConfig: SiteConfig = {
 		},
 		cjkFont: {
 			// 中日韩字体 - 作为回退字体
-			fontFamily: "萝莉体 第二版",
+			fontFamily: "MiSans-Bold",
 			fontWeight: "500",
-			localFonts: ["萝莉体 第二版.ttf"],
+			localFonts: ["MiSans-Bold.ttf"],
 			enableCompress: true, // 启用字体子集优化，减少字体文件大小
 		},
 	},
